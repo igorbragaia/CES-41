a04866b27a3fe16a9a7c32d94a9962fc5d8972e6
igorbragaia <igor.bragaia@gmail.com>
2019-07-13 07:35:32 -0300

    debuggando afnd to afd

--- list2/ex2/ex2.py
+++ list2/ex2/ex2.py
@@ -1,16 +1,29 @@
 from graphviz import Digraph
 from queue import Queue
 from pprint import pprint
+import itertools
 # esse codigo eh uma implementacao do algoritmo do slide 5c do prof Mokarzel
 
 
-class Node:
+def hashlist(list):
+    return ", ".join(sorted(list))
+
+
+class AfndNode:
     def __init__(self, left, right):
         self.left = left
         self.right = right
 
     def __str__(self):
         return "".join(self.left) + "->" + "".join(self.right)
+
+
+class AfdNode:
+    def __init__(self, closure):
+        self.closure = closure
+
+    def __str__(self):
+        return self.closure
 
 
 class Graph:
@@ -28,13 +41,57 @@ class Graph:
         self.add_node(to_node)
         self.graph[str(from_node)][str(to_node)] = label
 
+    def get_afd(self):
+        tmp = Graph()
+        gotos = self.get_gotos()
+        for Ii in gotos["gotos"]:
+            for Ij in gotos["gotos"][Ii]:
+                tmp.add_edge(Ii, Ij, gotos["gotos"][Ii][Ij])
+
+        afd = Graph()
+        queue = Queue(maxsize=10000)
+        queue.put(gotos["start"])
+        hash = {}
+        while not queue.empty():
+            front = queue.get()
+            for neighbor in tmp.graph[front]:
+                afd.add_edge(front, neighbor, tmp.graph[front][neighbor])
+                if neighbor not in hash:
+                    queue.put(neighbor)
+                    hash[neighbor] = True
+
+        return afd
+
+    def get_gotos(self):
+        closures = self.get_closures()
+
+        simbolos = set(itertools.chain(*[prod.left+prod.right for prod in self.nodes.values()]))
+        simbolos = [x for x in simbolos if x != '.']
+
+        gotos = {}
+        for key, value in closures.items():
+            for simbolo in simbolos:
+                J = []
+                for prod in value:
+                    for neighbor in self.graph[prod]:
+                        if self.graph[prod][neighbor] == simbolo:
+                            J.extend(closures[neighbor])
+                if len(J):
+                    if hashlist(value) not in gotos:
+                        gotos[hashlist(value)] = {}
+                    gotos[hashlist(value)][hashlist(J)] = simbolo
+        return {
+            "gotos": gotos,
+            "start": hashlist(closures["E'->.E"])
+        }
+
     def get_closures(self):
         closures = {}
         for node in self.graph:
-            closures[node] = self.get_closure(node)
+            closures[node] = self.__get_closure(node)
         return closures
 
-    def get_closure(self, node):
+    def __get_closure(self, node):
         closure = [node]
         queue = Queue(maxsize=10000)
         queue.put(node)
@@ -56,27 +113,23 @@ class Graph:
 
         f.view()
 
-
 prods = [
-    Node(["E'"], ["E"]),
-    Node(["E"], ["E", "+", "T"]),
-    Node(["E"], ["T"]),
-    Node(["T"], ["T", "*", "F"]),
-    Node(["T"], ["F"]),
-    Node(["F"], ["(", "E", ")"]),
-    Node(["F"], ["id"]),
+    AfndNode(["E'"], ["E"]),
+    AfndNode(["E"], ["E", "+", "T"]),
+    AfndNode(["E"], ["T"]),
+    AfndNode(["T"], ["T", "*", "F"]),
+    AfndNode(["T"], ["F"]),
+    AfndNode(["F"], ["(", "E", ")"]),
+    AfndNode(["F"], ["id"]),
 ]
-
-
-
 
 prods_ext = []
 for prod in prods:
     for i in range(len(prod.right)):
-        prods_ext.append(Node(prod.left, prod.right[:i] + ["."] + prod.right[i:]))
-    prods_ext.append(Node(prod.left, prod.right + ["."]))
+        prods_ext.append(AfndNode(prod.left, prod.right[:i] + ["."] + prod.right[i:]))
+    prods_ext.append(AfndNode(prod.left, prod.right + ["."]))
 
-graph = Graph()
+afnd = Graph()
 for prod in prods_ext:
     # primeiro tipo de transicao de AFND
     if prod.right[-1] != ".":
@@ -92,9 +145,9 @@ for prod in prods_ext:
                 transicao = prod.right[i+1]
                 next_prod_right.append(".")
                 i += 2
-        new_prod = Node(prod.left, next_prod_right)
+        new_prod = AfndNode(prod.left, next_prod_right)
 
-        graph.add_edge(prod, new_prod, transicao)
+        afnd.add_edge(prod, new_prod, transicao)
 
     # segundo tipo de transicao de AFND
     if prod.right[-1] != ".":
@@ -104,8 +157,8 @@ for prod in prods_ext:
             if prod.right[i] == ".":
                 prods_transicoes = [x for x in prods_ext if x.left[0] == prod.right[i+1] and x.right[0] == "." and str(prod) != str(x)]
                 for prod_transicao in prods_transicoes:
-                    graph.add_edge(prod, prod_transicao, "eps")
+                    afnd.add_edge(prod, prod_transicao, "eps")
             i += 1
 
-pprint(graph.get_closures())
-graph.view()
+afd = afnd.get_afd()
+afd.view()

bd158a3c3d82a1efff192eb310e5de8dabb2533e
igorbragaia <igor.bragaia@gmail.com>
2019-07-15 02:56:45 -0300

    finishing ex3 list1

--- list1/ex3/ex3.py
+++ list1/ex3/ex3.py
@@ -5,29 +5,29 @@ from pprint import pprint
 
 # exemplo lab
 
-# terminais = ["ID", "CTE", "int", "real", "+", "*", "=", ";", ",", "(", ")", "{", "}"]
-# nao_terminais = ["Prog", "Decls", "CmdComp", "Declaracao", "LDaux", "Tipo", "ListId", "LIaux", "ListCmd",
-#                  "Comando", "LCaux", "CmdAtrib", "Expressao", "Termo", "Eaux"]
-# producoes = {
-#     "Prog": [["Decls", "CmdComp"]],
-#     "Decls": [["Declaracao", "LDaux"]],
-#     "LDaux": [["eps"], ["Decls"]],
-#     "Declaracao": [["Tipo", "ListId", ";"]],
-#     "Tipo": [["int"], ["real"]],
-#     "ListId": [["ID", "LIaux"]],
-#     "LIaux": [["eps"], [",", "ListId"]],
-#     "CmdComp": [["{", "ListCmd", "}"]],
-#     "ListCmd": [["Comando", "LCaux"]],
-#     "LCaux": [["eps"], ["ListCmd"]],
-#     "Comando": [["CmdComp"], ["CmdAtrib"]],
-#     "CmdAtrib": [["ID", "=", "Expressao", ";"]],
-#     "Expressao": [["Termo", "Eaux"]],
-#     "Eaux": [["eps"], ["+", "Expressao"]],
-#     "Termo": [["(", "Expressao", ")"], ["ID"], ["CTE"]]
-# }
-
-# # # exemplo slide
-# terminais = ["(", ")", "id", "+", "*"]
+terminais = ["ID", "CTE", "int", "real", "+", "*", "=", ";", ",", "(", ")", "{", "}", "eps"]
+nao_terminais = ["Prog", "Decls", "CmdComp", "Declaracao", "LDaux", "Tipo", "ListId", "LIaux", "ListCmd",
+                 "Comando", "LCaux", "CmdAtrib", "Expressao", "Termo", "Eaux"]
+producoes = {
+    "Prog": [["Decls", "CmdComp"]],
+    "Decls": [["Declaracao", "LDaux"]],
+    "LDaux": [["eps"], ["Decls"]],
+    "Declaracao": [["Tipo", "ListId", ";"]],
+    "Tipo": [["int"], ["real"]],
+    "ListId": [["ID", "LIaux"]],
+    "LIaux": [["eps"], [",", "ListId"]],
+    "CmdComp": [["{", "ListCmd", "}"]],
+    "ListCmd": [["Comando", "LCaux"]],
+    "LCaux": [["eps"], ["ListCmd"]],
+    "Comando": [["CmdComp"], ["CmdAtrib"]],
+    "CmdAtrib": [["ID", "=", "Expressao", ";"]],
+    "Expressao": [["Termo", "Eaux"]],
+    "Eaux": [["eps"], ["+", "Expressao"]],
+    "Termo": [["(", "Expressao", ")"], ["ID"], ["CTE"]]
+}
+#
+# # exemplo slide
+# terminais = ["(", ")", "id", "+", "*", "eps"]
 # nao_terminais = ["E", "T", "E'", "T'", "F"]
 # producoes = {
 #     "E": [["T", "E'"]],
@@ -36,18 +36,26 @@ from pprint import pprint
 #     "T'": [["*", "F", "T'"], ["eps"]],
 #     "F": [["(", "E", ")"], ["id"]]
 # }
-#
-# questao 2 lista 2
-terminais = ["@", "+", "*", "(", ")", "a", ","]
-nao_terminais = ["E", "T", "F", "P", "L"]
-producoes = {
-    "E'": [["E"]],
-    "E": [["E", "+", "T"], ["T"]],
-    "T": [["T", "*", "F"], ["F"]],
-    "F": [["P", "@", "F"], ["P"]],
-    "P": [["(", "E", ")"], ["a"], ["a", "(", "L", ")"]],
-    "L": [["L", ",", "E"], ["E"]]
-}
+
+# terminais = ["i", "b", "e", "a", "eps"]
+# nao_terminais = ["S", "S'"]
+# producoes = {
+#     "S": [["i", "b", "S", "S'"], ["a"]],
+#     "S'": [["e", "S"], ["eps"]]
+# }
+
+
+# # questao 2 lista 2
+# terminais = ["@", "+", "*", "(", ")", "a", ","]
+# nao_terminais = ["E", "T", "F", "P", "L"]
+# producoes = {
+#     "E'": [["E"]],
+#     "E": [["E", "+", "T"], ["T"]],
+#     "T": [["T", "*", "F"], ["F"]],
+#     "F": [["P", "@", "F"], ["P"]],
+#     "P": [["(", "E", ")"], ["a"], ["a", "(", "L", ")"]],
+#     "L": [["L", ",", "E"], ["E"]]
+# }
 
 # # questao 2 lista 2 - exemplo slide
 # terminais = ["+", "*", "(", ")", "id"]
@@ -124,7 +132,7 @@ while keepgoing:
                     i += 1
                 else:
                     break
-print("primeiros:\n")
+print("Primeiros:\n")
 pprint(primeiros)
 
 # SEGUINTES
@@ -152,5 +160,54 @@ while keepgoing:
                     if add_to_seguintes(ys[i], seguintes[x]):
                         keepgoing = True
 
-print("\nseguintes:\n")
+print("\nSeguintes:\n")
 pprint(seguintes)
+
+tabela_de_prim_seg = {}
+# criar tabela
+for key, value in producoes.items():
+    for item in value:
+        tabela_de_prim_seg[key + "->" + "".join(item)] = {
+            "primalfa": primeiros[item[0]],
+            "primA": primeiros[key],
+            "segA": seguintes[key]
+        }
+
+with open("tabeladeprimseg.csv", "w") as f:
+    f.write("A->alfa#Prim(alfa)#Prim(A)#Seg(A)\n")
+    for prod in tabela_de_prim_seg:
+        f.write(prod + "#" + " ".join(tabela_de_prim_seg[prod]["primalfa"]) + "#" + " ".join(tabela_de_prim_seg[prod]["primA"]) + "#" + " ".join(tabela_de_prim_seg[prod]["segA"]) + "\n")
+
+
+tabela_de_prods = {}
+for key in tabela_de_prim_seg:
+    A = key.split('->')[0]
+    if A not in tabela_de_prods:
+        tabela_de_prods[A] = {}
+    for a in tabela_de_prim_seg[key]["primalfa"]:
+        if a not in tabela_de_prods[A]:
+            tabela_de_prods[A][a] = set()
+        tabela_de_prods[A][a].add(key)
+    if "eps" in tabela_de_prim_seg[key]["primalfa"]:
+        for b in tabela_de_prim_seg[key]["segA"]:
+            if b not in tabela_de_prods[A]:
+                tabela_de_prods[A][b] = set()
+            tabela_de_prods[A][b].add(key)
+        if "$" in tabela_de_prim_seg[key]["segA"]:
+            if "$" not in tabela_de_prods[key[0]]:
+                tabela_de_prods[A]["$"] = set()
+            tabela_de_prods[A]["$"].add(key)
+
+
+with open("tabeladeproducoes.csv", "w") as f:
+    keys = [x for x in terminais if x != "eps"] + ["$"]
+    f.write("# " + "# ".join(keys) + "\n")
+    for term in tabela_de_prods:
+        row = term + "# "
+        for key in keys:
+            if key in tabela_de_prods[term]:
+                row += "|".join(tabela_de_prods[term][key])
+            row += "# "
+
+        f.write(row + "\n")
+
